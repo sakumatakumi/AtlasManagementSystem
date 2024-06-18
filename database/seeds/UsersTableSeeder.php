4193e896e6ab13406f382c780b19b727e3d2314e
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-06-18 21:22:44 +0900

    ファーストコミットしたはずが反映されていなかったため、再度コミット

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            [
+                'over_name' => '若林',
+                'under_name' => '正恭',
+                'over_name_kana' => 'ワカバヤシ',
+                'under_name_kana' => 'マサヤス',
+                'mail_address' => 'wakasama@mail',
+                'sex' => '1',
+                'birth_day' => '1978-09-20',
+                'role' => '1',
+                'password' => bcrypt('wakasama')
+            ],
+        ]);
     }
 }
