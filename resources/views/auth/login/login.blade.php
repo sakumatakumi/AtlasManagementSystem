4c688e1eccf9ef739ad90c3952d21f7527475e0e
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-06-20 20:08:49 +0900

    atlasロゴの挿入。ブレード内でできる範囲のレイアウト。

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,10 +13,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&family=Oswald:wght@200&display=swap" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
+
 <body>
-  <form action="{{ route('loginPost') }}" method="POST">
-    <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-      <div class="border vh-50 w-25">
+  <form action="{{ route('loginPost') }}" method="POST" class="d-flex align-items-center justify-content-center vh-100">
+    <div class="d-flex flex-column align-items-center" style="width: 100%;">
+      <div class=" title mb-5">
+        <img src="{{ asset('image/atlas-black.png') }}" style="max-width: 200px;">
+      </div>
+      <div class="border p-4" style="width: 450px; max-width: 100%;">
         <div class="w-75 m-auto pt-5">
           <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
           <div class="border-bottom border-primary w-100">
@@ -32,7 +37,7 @@
           <input type="submit" class="btn btn-primary" value="ログイン">
         </div>
         <div class="text-center">
-          <a href="{{ route('registerView') }}">新規登録</a>
+          <a href="{{ route('registerView') }}">新規登録はこちら</a>
         </div>
       </div>
       {{ csrf_field() }}
@@ -42,4 +47,5 @@
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="{{ asset('js/register.js') }}" rel="stylesheet"></script>
 </body>
+
 </html>
