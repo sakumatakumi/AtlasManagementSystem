12d5e44c259db3c181438e8cacd3b0138c13d41f
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-06-24 22:07:20 +0900

    UserテーブルとSubjectｓテーブルのリレーション作成。 中間テーブルにタイムスタンプ導入。 対応問題ないか確認中。

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -15,7 +15,11 @@ class Subjects extends Model
         'subject'
     ];
 
-    public function users(){
-        return;// リレーションの定義
+
+    //基本的な型は第一引数は相手のモデル、第二はテーブル名、第三自分のIDが入る場所　、第四　相手のIDがはいるカラムを指定
+    //省略可能な場合もあり。laravel公式なドキュメントあり
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\Users\User', 'subject_users', 'subject_id', 'user_id')->withTimestamps(); // リレーションの定義
     }
 }
