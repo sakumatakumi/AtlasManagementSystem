d685f8b0ac8f6dc5188db19bbbf5619a2e1711c8
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-10-19 16:41:37 +0900

    いいねのカウント表示修正

--- app/Models/Posts/Like.php
+++ app/Models/Posts/Like.php
@@ -12,4 +12,9 @@ class Like extends Model
         'like_user_id',
         'like_post_id'
     ];
+
+    public function likeCounts($post_id)
+    {
+        return $this->where('like_post_id', $post_id)->get()->count();
+    }
 }
